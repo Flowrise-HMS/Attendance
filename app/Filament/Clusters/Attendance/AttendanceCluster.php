b478cdc9f92553aae0e6abbf5b2851287e4c1ef4
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:48 +0000

    feat: place the attendance cluster in the Operations sidebar group

--- app/Filament/Clusters/Attendance/AttendanceCluster.php
+++ app/Filament/Clusters/Attendance/AttendanceCluster.php
@@ -5,10 +5,15 @@ namespace Modules\Attendance\Filament\Clusters\Attendance;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Modules\Core\Enums\SidebarGroup;
 
 class AttendanceCluster extends Cluster
 {
     protected static ?string $slug = 'attendance-cluster';
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedClock;
+
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::Operations;
+
+    protected static ?int $navigationSort = 30;
 }
